1cae2b4d0ec1d33c6e6cced66d36279ed2c4bb4a
SangWooJeong <jsu4838@naver.com>
2019-08-02 11:17:32 +0900

    갤러리 링크 에셋 임포트

---
+++ BobAR/Assets/Plugins/SunShine Android Native Gallery/Scripts/TestGallery.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGallery : MonoBehaviour
+{
+
+    [SerializeField]
+    SunshineNativeGalleryHandler _sunshineScript;
+
+
+    public void OpenGallery()
+    {
+
+        _sunshineScript.OpenGallery((bool success, List<string> paths) =>
+        {
+            if (success)
+            {
+               
+                // paths = file location or url of all picked items
+                //Showing the picked Item.. You can make your own gallery by yourself
+                FindObjectOfType<Gallery>().PrepareGallery(paths);
+                FindObjectOfType<GalleryManager>().ShowCrossBtn();
+            }
+        });
+
+
+    }
+
+
+}
