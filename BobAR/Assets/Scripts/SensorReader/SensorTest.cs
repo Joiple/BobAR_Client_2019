c5328c43c431da1a2cf82a9988fe20fd53a32e35
정상우 <jsu4838@naver.com>
2019-08-24 01:02:07 +0900

    센서 리더 추가

---
+++ BobAR/Assets/Scripts/SensorReader/SensorTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using SensorReader;
+using TMPro;
+
+public class SensorTest : MonoBehaviour
+{
+    public TextMeshProUGUI view;
+    // Use this for initialization
+    void Start()
+    {
+        SensorReaderLibWrapper.Initialize();
+        SensorReaderLibWrapper.Start(SensorType.Light);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        view.text =$"{SensorReaderLibWrapper.Get(SensorType.Light, 0):F5}";
+    }
+
+    void OnDestroy()
+    {
+        SensorReaderLibWrapper.End(SensorType.Light);
+    }
+}
