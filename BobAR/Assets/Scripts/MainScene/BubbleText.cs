1ab0bc21f5fbd05aa1108013bdcaecc38da7a023
SangWooJeong <jsu4838@naver.com>
2019-10-08 16:22:28 +0900

    리뷰 버블메세지 추가

---
+++ BobAR/Assets/Scripts/MainScene/BubbleText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using Common.Dummies;
+using UnityEngine;
+
+namespace MainScene {
+
+    public class BubbleText :MonoBehaviour {
+        [SerializeField] private Transform listGameObject;
+        [SerializeField] private BubbleTextComponent prefab;
+        public void Start() => Initialize();
+        public void Initialize() {
+            StartCoroutine(RandomGeneration());
+        }
+        private IEnumerator RandomGeneration() {
+            while (true) {
+                Instantiate(prefab, listGameObject).Initialize((int) (Random.value*(DummyContainer.instance.reviews.Length-1)));
+                yield return new WaitForSeconds(Random.value * 3 + .5f);
+            }
+        }
+    }
+
+}
---
+++ BobAR/Assets/Scripts/MainScene/BubbleTextComponent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using Common.Dummies;
+using CustomSceneManagement;
+using DataManagement;
+using NormalScene;
+using TMPro;
+using UnityEngine;
+
+namespace MainScene {
+
+    public class BubbleTextComponent : MonoBehaviour {
+        private int index;
+        [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private Animator animator;
+        [SerializeField] private float maximumLifeTime=5f;
+        public bool needToRemove = false;
+        
+        public void Initialize(int input) {
+            index = input;
+            text.text = DummyContainer.instance.reviewDB[DummyContainer.instance.reviews[input].key].content;
+            StartCoroutine(LifeCycle());
+        }
+        private IEnumerator LifeCycle() {
+            animator.SetTrigger("init");
+            float t = maximumLifeTime;
+            while (t > 0 || needToRemove) {
+                t -= Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            animator.SetTrigger("end");
+            yield return new WaitForSeconds(2f);
+            Destroy(gameObject);
+        }
+        public void ClickMenu() {
+            DataStorage.instance.AddItem(DataStorageKeyset.InitialScene, PageType.ReviewDetailPage);
+            DataStorage.instance.AddItem(DataStorageKeyset.NextReview, index);
+            CustomSceneManager.instance.LoadScene(1);
+        }
+    }
+
+}
