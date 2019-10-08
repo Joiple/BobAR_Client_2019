af32d5ddc546bd3b1733ddb6cd34df976ed1fcc7
SangWooJeong <jsu4838@naver.com>
2019-10-08 12:43:04 +0900

    타이틀씬 추가
    리소스 추가

---
+++ BobAR/Assets/Scripts/TitleScene/TitleSceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using CustomSceneManagement;
+using UnityEngine;
+
+namespace TitleScene {
+
+    public class TitleSceneManager : MonoBehaviour {
+        public void Start() {
+            StartCoroutine(Coroutine());
+        }
+        public IEnumerator Coroutine() {
+            yield return new WaitForSeconds(2f);
+            //TODO Toast
+            CustomSceneManager.instance.LoadScene(0);
+
+        }
+    }
+
+}
